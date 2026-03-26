f0e67555a7abbd26e2245d75769b42dd38c48803
Abhijeetkumar777 <abhijeetmehtaji@gmail.com>
2026-03-26 19:22:19 +0530

    fix backend and frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:5000/api";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "https://spend-pilot-backend.onrender.com/api";
 
 const API = axios.create({
   baseURL,
